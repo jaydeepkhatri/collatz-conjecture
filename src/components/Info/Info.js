fb58c6fb1a2d8291d25816290d6f762c78e88e24
Jaydeep Khatri <jaydeepkhatri79@gmail.com>
2023-01-06 11:10:51 +0530

    ✏ Update style

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -3,8 +3,8 @@ const Info = ({hailStoneNumbers}) => {
         <>
             <div className="md:mx-auto w-full max-w-6xl pt-20 text-left">
                 <div className="bg-gray-100 p-4">
-                    <p>Hailstone Numbers : {hailStoneNumbers.map((number, i) => (<span key={i}>{number}, </span>))}</p>
-                    <p className="mt-2">Total Hailstone numbers: {hailStoneNumbers.length}</p>
+                    <p><span className="font-bold">Hailstone Numbers</span>: {hailStoneNumbers.map((number, i) => (<span key={i}>{number}, </span>))}</p>
+                    <p className="mt-2"><span className="font-bold">Total Hailstone numbers:</span> {hailStoneNumbers.length}</p>
                 </div>
             </div>
         </>
